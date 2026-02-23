ecf7ee1259cc4419c56afa6a4f6bd9e1f0ba9936
GLade <96268794+fiotedeluz@users.noreply.github.com>
2026-02-23 20:56:59 -0300

    Create aulaGalo6.1.js

---
+++ facudadi/aulaGalo6.1.js
@@ -0,0 +1,8 @@
+console.log ("qual o seu nome?");
+let nome = prompt();
+console.log ("bem vindo(a)" + nome + "!");
+
+n = 19.2;
+console.log (typeof(n));
+
+// essa faculdade ta me matando isso e conteudo de primeiro dia e eu to fazendo denovo q odioa ahhhhhhhhhhh
