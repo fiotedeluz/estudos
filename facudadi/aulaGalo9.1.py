e8a87e5a072a9239fa5daa234161c02f48294694
GLade <96268794+fiotedeluz@users.noreply.github.com>
2026-03-03 21:50:59 -0300

    Create aulaGalo9.1.py

---
+++ facudadi/aulaGalo9.1.py
@@ -0,0 +1,34 @@
+
+
+
+
+#aulinha d pyton revendo conceitos de for e while
+
+
+
+#i = 1
+#r = 0
+#while i < 5:
+#    r = r + i
+#    i += 1
+#    print (r)
+    
+
+
+#while i <= 20:
+#    if i % 2 != 0:
+#        i += 1
+#    else:
+#        print (i)
+#        i = i +1
+
+#for i in range (1, 40, 4):
+#    print (i)
+
+#for i in range (2, 20):
+#while i < 20:
+#    if i % i == 0 and i % 2 == 0:
+#        i += 1
+#    else:
+#        print (i)
+#        i += 1
