093812f75da85b1276288e67630ca901b1a8d3d9
GLade <96268794+fiotedeluz@users.noreply.github.com>
2026-03-09 20:28:35 -0300

    Create aulaGalo10.0.js

---
+++ facudadi/aulaGalo10.0.js
@@ -0,0 +1,7 @@
+
+// taboada simples implementando for loop
+
+let numero = prompt ("Digite um numero para ver a tabuada");
+for ( let i = 1; i <= 10; i++) {
+    console.log(numero + " x " + i + " = " + (numero * i));
+}
