892a33b39725e2ce410765c305c022bf0d112d75
GLade <96268794+fiotedeluz@users.noreply.github.com>
2026-03-10 21:04:48 -0300

    Create aulaGalo11.0.py

---
+++ facudadi/aulaGalo11.0.py
@@ -0,0 +1,21 @@
+
+
+
+
+# teste simples do uso de for com mais condicionais aplicando enumerate e selecionando partes especificas do codigo
+ 
+#tx = "paralelepipedo"
+
+#for i, c in enumerate(tx):
+#    if c == 'a' or c == 'e':
+#        print(f"a vogal {c} aparece na {i} posição")
+#    else:
+#        continue
+
+
+# primeiros passos curtos em funções pytonianas
+
+#def imprimir_mensagem(dic, cur):
+#    print(f"minha primeira função aprendida na disiplina {dic}, no curso de {cur}")
+#
+#imprimir_mensagem("PY", "ADS")
