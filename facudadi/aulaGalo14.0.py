abc3e07f42d7fc74e6a4ea4eccdfa033d552c281
GLade <96268794+fiotedeluz@users.noreply.github.com>
2026-03-24 20:07:02 -0300

    Create aulaGalo14.0.py

---
+++ facudadi/aulaGalo14.0.py
@@ -0,0 +1,16 @@
+
+#entendendo propriedades de texto e usos diversos
+
+txt = "aprendendo python na aula de quarta feira"
+
+print(f"tamanho do texto = {len(txt)}")
+print(f"python em texto = {'python' in txt}")
+print(f"quantidade de Y no texto = {txt.count('y')}")
+print(f"as 5 primeiras letras são = {txt[0:6]}")
+
+print(txt.upper)
+print(txt.replace('a', ei))
+
+palavras = txt.split()
+print (f"palavras = {palavras}")
+print (f"txt tem {len(palavras)} palavras")
