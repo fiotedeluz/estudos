4e94372ab9dde6f957dc0339b6233a35a3289bf3
GLade <96268794+fiotedeluz@users.noreply.github.com>
2026-09-03 22:04:44 -0300

    Create main.java

---
+++ facudadi/ProgramaçãoWeb/Aula001/calculadora/main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) {
+        calculadora calculadora = new calculadora();
+        int resultado = calculadora.multiplica(10, 5);
+        System.out.println("resultado: " + resultado);
+    }
+}
