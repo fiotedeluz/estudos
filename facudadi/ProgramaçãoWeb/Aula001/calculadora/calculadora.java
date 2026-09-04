c97ecccbfa54fccbb190796a8fe983e6a03699db
GLade <96268794+fiotedeluz@users.noreply.github.com>
2026-09-03 22:05:15 -0300

    Create calculadora.java

---
+++ facudadi/ProgramaçãoWeb/Aula001/calculadora/calculadora.java
@@ -0,0 +1,23 @@
+
+public class calculadora {
+    public int soma(int numero1, int numero2){
+        int resultado = numero1 + numero2;
+        return resultado;
+        
+    }
+
+    public int subtrai (int numero1, int numero2){
+        int resultado = numero1 - numero2;
+        return resultado;
+    }
+
+        public int multiplica (int numero1, int numero2){
+        int resultado = numero1 * numero2;
+        return resultado;
+    }
+
+        public int divide (int numero1, int numero2){
+        int resultado = numero1 / numero2;
+        return resultado;
+    }
+}
