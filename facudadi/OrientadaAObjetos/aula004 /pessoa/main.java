53d2ff81ec3fd3290fb9ba60133b201f8e960114
GLade <96268794+fiotedeluz@users.noreply.github.com>
2026-09-17 22:02:47 -0300

    Add main class to create and display Pessoa objects

---
+++ facudadi/OrientadaAObjetos/aula004 /pessoa/main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {    
+    Pessoa pessoa1 = new Pessoa(
+      "jose",
+      "000000000-00" 
+       );
+
+    Pessoa pessoa2 = new Pessoa(
+      "robson",
+      "theLegend-00" 
+       );
+
+    pessoa1.exibirDados();
+    pessoa2.exibirDados();
+    }
+}
